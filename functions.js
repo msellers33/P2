c44846331f7c339637e8cf89b826e5715fb8601a
msellers33 <msellers@uoregon.edu>
2015-04-16 14:13:55 -0700

    Add Amazeriffic FAQ page

---
+++ functions.js
@@ -0,0 +1,55 @@
+//P2 req. 2-A
+var isArmstrong = function(k){
+	"use strict";
+	k = k.toString();
+	var m= k.length;
+	var number, sum = 0;
+	for( var i = 0; i < m; ++i){
+		number= Number(k[i]);
+		var multi = Math.pow(number,m);
+		sum = sum + multi;
+	}
+	if (Number(k)==sum){
+		return true;
+	}
+	else{return false}
+}
+
+//P2 req. 2-B
+var allArmstrongs=function(){
+	var result = "";
+	 //generate
+
+	 for(var k=1; k<=99999; ++k){
+	 	//test
+	 	if (isArmstrong(k)){
+	 	result = result + k + ' ';}
+	 }
+	 return result;
+} 
+
+//P2 req. 2-C
+var allSubstrings1 = function(s){
+	var result ="";
+	for(var i = 0; i< s.length; i++) {
+		for(var j = i+1; j<=s.length;j++) {
+			var str = s.substring(i,j);
+            result = result + str + ", ";
+		}
+	}
+	return result;
+}
+
+//P2 req. 2-D
+var allSubstrings2 = function(s){
+	var result ="";
+	for(var i = 0; i< s.length; i++) {
+		for(var j = i+1; j<=s.length;j++) {
+			var str = s.substring[i,j];
+            result = result + str + " " , " ";
+		}
+	}
+	return result;
+}
+
+
